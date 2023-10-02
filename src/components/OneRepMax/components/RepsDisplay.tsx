50bbf2ace74826d80fc7bce3b441a8f9b75a17de
Mehdi Salemi <mehdisalemi@Mehdis-MacBook-Air.local>
2023-10-01 22:06:39 -0400

    add tabs to really implement later

--- src/components/OneRepMax/components/RepsDisplay.tsx
+++ src/components/OneRepMax/components/RepsDisplay.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import { useState, useCallback } from "react";
 
-import { LegacyStack, LegacyCard } from "@shopify/polaris";
+import {
+  SkeletonBodyText,
+  LegacyCard,
+  Tabs,
+  TextContainer,
+  SkeletonDisplayText,
+  Text,
+} from "@shopify/polaris";
 
 import { DEFAULT_REPS } from "../../../utils/constants/contants";
 
@@ -18,9 +25,49 @@ export function RepsDisplay({ oneRepMax }: { oneRepMax: number }) {
     };
   });
 
+  const [selected, setSelected] = useState(0);
+
+  const handleTabChange = useCallback(
+    (selectedTabIndex: number) => setSelected(selectedTabIndex),
+    []
+  );
+
+  const tabs = [
+    {
+      id: "bench-press",
+      content: "Bench Press",
+      panelID: "bench-press-content-1",
+    },
+    {
+      id: "pull-up",
+      content: "Pull Ups",
+      panelID: "pull-ups-content-1",
+    },
+  ];
+
+  console.log("selected", tabs[selected]);
+
   return (
     <div>
-      <RepsDisplayTable data={orm_ranges} oneRepMax={oneRepMax} />
+      <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
+        <LegacyCard.Section title={tabs[selected].content}>
+          <p>Tab {selected} selected</p>
+        </LegacyCard.Section>
+      </Tabs>
+      {tabs[selected].id === "bench-press" && (
+        <RepsDisplayTable data={orm_ranges} oneRepMax={oneRepMax} />
+      )}
+      {tabs[selected].id === "pull-up" && (
+        <LegacyCard.Section>
+          <TextContainer>
+            <Text variant="bodyMd" fontWeight="bold" as="span">
+              Coming soon...
+            </Text>
+            <SkeletonDisplayText size="small" />
+            <SkeletonBodyText />
+          </TextContainer>
+        </LegacyCard.Section>
+      )}
     </div>
   );
 }
